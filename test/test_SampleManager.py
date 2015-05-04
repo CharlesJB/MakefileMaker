27316c1882d8742f1ee5ae7806565c5017d8a8d0
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2015-05-04 14:37:20 -0400

    New tests infrastructure for SampleManager
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

---
+++ test/test_SampleManager.py
@@ -0,0 +1,67 @@
+from nose.tools import *
+from lib.SampleManager import *
+
+class NullWriter:
+    def write(self, s):
+        pass
+sys.stderr = NullWriter()
+
+# TESTS: SampleManager
+
+VALID_SAMPLESHEET_FULL = "raw_data/valid_samplesheet.txt"
+VALID_SAMPLESHEET_IDENTICAL_NAMES = "raw_data/valid_samplesheet_identical_names.txt"
+VALID_SAMPLESHEET_NO_FASTQ2_PARTIAL = "raw_data/valid_samplesheet_no_fastq2_partial.txt"
+VALID_SAMPLESHEET_NO_FASTQ2 = "raw_data/valid_samplesheet_no_fastq2.txt"
+INVALID_SAMPLESHEET_NO_FASTQ1 = "raw_data/invalid_samplesheet_no_fastq1.txt"
+INVALID_SAMPLESHEET_NO_FASTQ2 = "raw_data/invalid_samplesheet_no_fastq2.txt"
+INVALID_SAMPLESHEET_NO_NAME = "raw_data/invalid_samplesheet_no_name.txt"
+VALID_INI = "raw_data/valid.ini"
+
+EXP_VALUES_FASTQ1_FULL = [["raw_data/a1_1.fastq.gz"], ["raw_data/a2_1.fastq.gz"]]
+EXP_VALUES_FASTQ2_FULL = [["raw_data/a1_2.fastq.gz"], ["raw_data/a2_2.fastq.gz"]]
+EXP_VALUES_FASTQ1_IDENTICAL_NAMES = [["raw_data/a1_1.fastq.gz", "raw_data/a2_1.fastq.gz"]]
+EXP_VALUES_FASTQ2_IDENTICAL_NAMES = [["raw_data/a1_2.fastq.gz", "raw_data/a2_2.fastq.gz"]]
+EXP_VALUES_FASTQ2_PARTIAL = [["raw_data/a1_2.fastq.gz"], ['']]
+EXP_VALUES_NO_FASTQ2 = [[''], ['']]
+EXP_KEYS_DIFFERENT = ['test1', 'test2']
+EXP_KEYS_SAME = ['test1']
+
+def test_samplemanager_constructor_valid_sample_sheet():
+    sm = SampleManager(VALID_SAMPLESHEET_FULL)
+    eq_(sm.raw_files_r1.keys(), EXP_KEYS_DIFFERENT)
+    eq_(sm.raw_files_r1.values(), EXP_VALUES_FASTQ1_FULL)
+    eq_(sm.raw_files_r2.keys(), EXP_KEYS_DIFFERENT)
+    eq_(sm.raw_files_r2.values(), EXP_VALUES_FASTQ2_FULL)
+
+def test_samplemanager_constructor_valid_sample_sheet_no_fastq2():
+    sm = SampleManager(VALID_SAMPLESHEET_NO_FASTQ2)
+    eq_(sm.raw_files_r1.keys(), EXP_KEYS_DIFFERENT)
+    eq_(sm.raw_files_r1.values(), EXP_VALUES_FASTQ1_FULL)
+    eq_(sm.raw_files_r2.keys(), EXP_KEYS_DIFFERENT)
+    eq_(sm.raw_files_r2.values(), EXP_VALUES_NO_FASTQ2)
+
+def test_samplemanager_constructor_valid_sample_sheet_no_fastq2_partial():
+    sm = SampleManager(VALID_SAMPLESHEET_NO_FASTQ2_PARTIAL)
+    eq_(sm.raw_files_r1.keys(), EXP_KEYS_DIFFERENT)
+    eq_(sm.raw_files_r1.values(), EXP_VALUES_FASTQ1_FULL)
+    eq_(sm.raw_files_r2.keys(), EXP_KEYS_DIFFERENT)
+    eq_(sm.raw_files_r2.values(), EXP_VALUES_FASTQ2_PARTIAL)
+
+def test_samplemanager_constructor_valid_sample_sheet_identical_names():
+    sm = SampleManager(VALID_SAMPLESHEET_IDENTICAL_NAMES)
+    eq_(sm.raw_files_r1.keys(), EXP_KEYS_SAME)
+    eq_(sm.raw_files_r1.values(), EXP_VALUES_FASTQ1_IDENTICAL_NAMES)
+    eq_(sm.raw_files_r2.keys(), EXP_KEYS_SAME)
+    eq_(sm.raw_files_r2.values(), EXP_VALUES_FASTQ2_IDENTICAL_NAMES)
+
+@raises(SystemExit)
+def test_samplemanager_constructor_invalid_sample_sheet_no_name():
+    SampleManager(INVALID_SAMPLESHEET_NO_NAME)
+
+@raises(SystemExit)
+def test_samplemanager_constructor_invalid_sample_sheet_no_fastq1():
+    SampleManager(INVALID_SAMPLESHEET_NO_FASTQ1)
+
+@raises(SystemExit)
+def test_samplemanager_constructor_invalid_sample_sheet_no_fastq1():
+    SampleManager(INVALID_SAMPLESHEET_NO_FASTQ2)
