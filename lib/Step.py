8677a0311d929ea691023cf0d3f2c2d007e9293a
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2015-06-15 17:09:44 -0400

    Working version of Step
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- lib/Step.py
+++ lib/Step.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # encoding: utf-8
 # author: Charles Joly Beauparlant
-# 2015-04-30
+# 2015-05-11
 
 import sys
 import os
@@ -10,28 +10,63 @@ import ConfigParser
 from lib.FileList import *
 
 class Step:
-    def __init__(self):
-        self.name = self.get_name()
-        self.config_files = []
+    def __init__(self, config_files):
         self.params = {}
         self._set_default_params()
-        self.dependency_names = []
-        self._set_dependency_names()
 
-    def add_config_file(self, config_file):
-        if self._check_string(config_file):
-            self.config_files.append(config_file)
+        # Step specific values, to set in _set_step_specific_values
+        self.name = ""
+        self.dir_name = ""
+        self.merge_status = False
+        self.pair_status = False
+        self.keep_pair_together_status = False
+        self._set_step_specific_values()
 
-    # dependencies is a dict of FileList
-    def produce_recipe(self, dependencies, outputs):
-        self._validate_params(dependencies, outputs)
+        # Prepare config file
+        self.config_files = config_files
         self._parse_config()
-        recipe = ''
-        recipe += " ".join(outputs) + ": "
-        for file_list in dependencies.values():
-            recipe += " ".join(file_list.unlist())
-        recipe += "\n"
-        recipe += self._get_command(dependencies, outputs)
+
+        # Check if object is still valid
+        self._valid()
+
+    def get_name(self):
+        return(self.name)
+
+    def get_dir_name(self):
+        return(self.dir_name)
+
+    def get_merge_status(self):
+        return(self.merge_status)
+
+    def get_pair_status(self):
+        return(self.pair_status)
+
+    def get_keep_pair_together_status(self):
+        return(self.keep_pair_together_status)
+
+    # To implement in each Step
+    def get_step_specific_variables(self):
+        pass
+
+    # To implement in each Step
+    def get_target(self):
+        pass
+
+    # To implement in each Step
+    def get_phony_target(self):
+        pass
+
+    # inputs and outputs are list of FileList
+    def produce_recipe(self, inputs, outputs):
+        self._validate_params(inputs, outputs)
+        # Target
+        target = self._list_files(outputs)
+        dependencies  = self._list_files(inputs)
+        if len(dependencies) > 0:
+            recipe = target + ": " + dependencies + "\n"
+        else:
+            recipe = target + ":\n"
+        recipe += self._get_command(inputs, outputs)
         return(recipe)
 
     def _parse_config(self):
@@ -39,7 +74,7 @@ class Step:
         if len(self.config_files) > 0:
             for config_file in self.config_files:
                 config_parser.read(config_file)
-            if self.name in config.sections():
+            if self.name in config_parser.sections():
                 for param in self.params.keys():
                     try:
                         new_param = config_parser.get(self.name, param)
@@ -47,33 +82,65 @@ class Step:
                     except ConfigParser.NoOptionError:
                         pass
 
-    def _validate_params(self, dependencies, outputs):
-        msg = "Step: _validate_params: "
+    def _list_files(self, file_lists):
+        result = []
+        for file_list in file_lists:
+            result += file_list.unlist()
+        return(" ".join(result))
+
+    def _valid(self):
         error = False
-        if not isinstance(dependencies, dict):
-            msg += "dependencies should be a dict.\n"
+        msg = "Step: _valid: "
+        if not isinstance(self.config_files, list):
+            msg += "config_files should be a list.\n"
             error = True
+        elif len(self.config_files) < 1:
+            msg += "config_files should have at least one entry"
         else:
-            for name in dependencies.keys():
-                if name not in self.dependency_names:
-                    msg += name + " is not an exepted dependency for step "
-                    msg += self.step_name + "\n"
-                    error = True
-            for file_list in dependencies.values():
-                if not isinstance(file_list, FileList):
-                    msg += "dependencies entries must be FileList\n"
-                    error = True
-        if not isinstance(outputs, list):
-            msg += "outputs should be a list.\n"
-            error = True
-        else:
-            for output in outputs:
-                if not self._check_string(output):
-                    msg += str(output) + " is incorrectly formatted.\n"
+            for config_file in self.config_files:
+                if not self._check_string(config_file):
+                    msg += "A config file entry is not a valid string."
                     error = True
         if error == True:
             sys.stderr.write(msg)
             sys.exit(1)
+
+    def _validate_params(self, inputs, outputs):
+        msg = "Step: _validate_params: "
+        error = False
+        if not isinstance(inputs, list):
+            msg += "inputs/outputs should be list."
+            error = True
+        else:
+            if not isinstance(inputs, list):
+                msg += "inputs should be a list."
+                error = True
+            else:
+                for inpt in inputs:
+                    if not isinstance(inpt, FileList):
+                        msg += "inputs must be FileList."
+                        error = True
+            if not isinstance(outputs, list):
+                msg += "outputs should be a list."
+                error = True
+            elif len(outputs) < 1:
+                msg += "outputs should have at least one entry"
+            else:
+                for output in outputs:
+                    if not isinstance(output, FileList):
+                        msg += "outputs must be FileList."
+                        error = True
+                    if self.get_pair_status() and output.get_paired_status():
+                        msg += "outputs cannot be paired when pair status is True."
+                        error = True
+                    if self.get_merge_status() and output.get_file_count() > 1:
+                        msg += "outputs FileList cannot contain more than 1 file when merge status is True."
+                        error = True
+        if error == True:
+            sys.stderr.write(msg)
+            sys.exit(1)
+
+        self._validate_param_step_specific(inputs, outputs)
 
     def _check_string(self, string):
         if not isinstance(string, basestring):
@@ -83,18 +150,45 @@ class Step:
         return(True)
 
 class DummyStep(Step):
-    def get_name(self):
-        return("DummyStep")
+    # To implement in each Step
+    def get_step_specific_variables(self):
+        return("ABC=DEF")
 
+    # To implement in each Step
+    def get_target(self):
+        return("TARGET_DUMMY=dummy.txt")
+
+    # To implement in each Step
+    def get_phony_target(self):
+        return("dummy: $(TARGET_DUMMY)")
+
+    # To implement in each Step
+    def _set_step_specific_values(self):
+        self.name = "DummyStep"
+        self.dir_name = "Dummy"
+        self.merge_status = True
+        self.pair_status = True
+        self.keep_pair_together_status = False
+
+    # To implement in each Step
     def _get_command(self, dependencies, outputs):
         command = ""
         command += "\t@echo $@\n"
         command += "\t@echo $^\n"
         return(command)
 
+    # To implement in each Step
     def _set_default_params(self):
-        self.params['param1'] = 1
-        self.params['param2'] = 2
+        self.params['dummy_param_1'] = 'dummy_param_default_1'
+        self.params['dummy_param_2'] = 'dummy_param_default_2'
 
-    def _set_dependency_names(self):
-        self.dependency_names.append('dependency_name_1')
+    # To implement in each Step, if necessary
+    def _validate_param_step_specific(self, inputs, outputs):
+        error=False
+        msg = "DummyStep - _validate_param_step_specific: "
+        if len(outputs) != 1:
+            msg += "outputs must be of length 1."
+            error = True
+        if error == True:
+            sys.stderr.write(msg)
+            sys.exit(1)
--- test/test_Step.py
+++ test/test_Step.py
@@ -4,49 +4,111 @@ from lib.FileList import *
 
 VALID_CONFIG_FILE_1 = "raw_data/valid_dummy_1.ini"
 VALID_CONFIG_FILE_2 = "raw_data/valid_dummy_2.ini"
-DUMMY_PARAM_1_1 = 'dummy_param_1'
-DUMMY_PARAM_2_1 = 'dummy_param_2'
-DUMMY_PARAM_1_2 = 'dummy_param_3'
-DUMMY_PARAM_2_2 = 'dummy_param_4'
 
-FILE_1_R1 = 'dependency_1_R1'
-FILE_1_R2 = 'dependency_1_R2'
-VALID_DEPENDENCY_NAME_1 = 'dependency_name_1'
-VALID_DEPENDENCIES_PAIRED = {}
-VALID_FILE_LIST_1 = FileList([[FILE_1_R1, FILE_1_R2]], VALID_DEPENDENCY_NAME_1)
-VALID_DEPENDENCIES_PAIRED[VALID_DEPENDENCY_NAME_1] = VALID_FILE_LIST_1
-
-VALID_OUTPUTS_1 = ['valid_output_1']
-
-EXP_RECIPE_1 = VALID_OUTPUTS_1[0] + ": "
-EXP_RECIPE_1 += FILE_1_R1 + " "
-EXP_RECIPE_1 += FILE_1_R2 + "\n"
-EXP_RECIPE_1 += "\t@echo $@\n"
-EXP_RECIPE_1 += "\t@echo $^\n"
+FILE_1_R1 = '1_R1'
+FILE_1_R2 = '1_R2'
+FILE_2_R1 = '2_R1'
+FILE_2_R2 = '2_R2'
+FILE_LIST_UNPAIRED_1 = FileList([[FILE_1_R1, '']])
+#FILE_LIST_UNPAIRED_2 = FileList([[FILE_2_R1, '']])
+FILE_LIST_PAIRED_1 = FileList([[FILE_1_R1, FILE_1_R2]])
+FILE_LIST_PAIRED_2 = FileList([[FILE_2_R1, FILE_2_R2]])
+FILE_LIST_2_FILES = FileList([[FILE_1_R1, ''], [FILE_2_R1, '']])
 
 def test_dummy_step_constructor():
-    ds = DummyStep()
+    ds = DummyStep([VALID_CONFIG_FILE_1])
     eq_(ds.name, "DummyStep")
     eq_(isinstance(ds.params, dict), True)
     eq_(len(ds.params), 2)
-    eq_(ds.params['param1'], 1)
-    eq_(ds.params['param2'], 2)
-    eq_(ds.dependency_names, [VALID_DEPENDENCY_NAME_1])
-    eq_(ds.config_files, [])
-
-def test_dummy_step_get_name():
-    ds = DummyStep()
-    eq_(ds.get_name(), "DummyStep")
-
-def test_dummy_step_add_config_file():
-    ds = DummyStep()
-    eq_(ds.config_files, [])
-    ds.add_config_file(VALID_CONFIG_FILE_1)
+    eq_(ds.params['dummy_param_1'], 'dummy_param_1_1')
+    eq_(ds.params['dummy_param_2'], 'dummy_param_1_2')
     eq_(ds.config_files, [VALID_CONFIG_FILE_1])
-    ds.add_config_file(VALID_CONFIG_FILE_2)
+
+def test_dummy_step_constructor_2_config_files():
+    ds = DummyStep([VALID_CONFIG_FILE_1, VALID_CONFIG_FILE_2])
+    eq_(ds.name, "DummyStep")
+    eq_(isinstance(ds.params, dict), True)
+    eq_(len(ds.params), 2)
+    eq_(ds.params['dummy_param_1'], 'dummy_param_2_1')
+    eq_(ds.params['dummy_param_2'], 'dummy_param_2_2')
     eq_(ds.config_files, [VALID_CONFIG_FILE_1, VALID_CONFIG_FILE_2])
 
-def test_dummy_produce_recipe_valid_params():
-    ds = DummyStep()
-    recipe = ds.produce_recipe(VALID_DEPENDENCIES_PAIRED, VALID_OUTPUTS_1)
-    eq_(recipe, EXP_RECIPE_1)
+def test_dummy_step_constructor_no_config_files():
+    ds = DummyStep([])
+    eq_(ds.name, "DummyStep")
+    eq_(isinstance(ds.params, dict), True)
+    eq_(len(ds.params), 2)
+    eq_(ds.params['dummy_param_1'], 'dummy_param_default_1')
+    eq_(ds.params['dummy_param_2'], 'dummy_param_default_2')
+    eq_(ds.config_files, [])
+
+@raises(SystemExit)
+def test_dummy_step_constructor_invalid_config_file():
+    DummyStep(VALID_CONFIG_FILE_1)
+
+def test_dummy_step_get_name():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_name(), "DummyStep")
+
+def test_dummy_step_get_dir_name():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_dir_name(), "Dummy")
+
+def test_dummy_step_get_pair_status():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_pair_status(), True)
+
+def test_dummy_step_get_merge_status():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_merge_status(), True)
+
+def test_dummy_step_get_keep_pair_together_status():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_keep_pair_together_status(), False)
+
+def test_dummy_step_get_step_specific_variables():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_step_specific_variables(), "ABC=DEF")
+
+def test_dummy_step_get_target():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_target(), "TARGET_DUMMY=dummy.txt")
+
+def test_dummy_step_get_phony_target():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    eq_(ds.get_phony_target(), "dummy: $(TARGET_DUMMY)")
+
+def test_dummy_step_produce_recipe():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    recipe = ds.produce_recipe([FILE_LIST_PAIRED_2], [FILE_LIST_UNPAIRED_1])
+    eq_(recipe, '1_R1: 2_R1 2_R2\n\t@echo $@\n\t@echo $^\n')
+
+def test_dummy_step_produce_recipe_empty_input():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    recipe = ds.produce_recipe([], [FILE_LIST_UNPAIRED_1])
+    eq_(recipe, '1_R1:\n\t@echo $@\n\t@echo $^\n')
+
+@raises(SystemExit)
+def test_dummy_step_produce_recipe_invalid_input_length():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    recipe = ds.produce_recipe([FILE_LIST_PAIRED_1], [FILE_LIST_2_FILES])
+
+@raises(SystemExit)
+def test_dummy_step_produce_recipe_invalid_input_class():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    recipe = ds.produce_recipe(FILE_LIST_PAIRED_1, [FILE_LIST_UNPAIRED_1])
+
+@raises(SystemExit)
+def test_dummy_step_produce_recipe_invalid_output_class():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    recipe = ds.produce_recipe([FILE_LIST_PAIRED_1], FILE_LIST_UNPAIRED_1)
+
+@raises(SystemExit)
+def test_dummy_step_produce_recipe_invalid_output_merge():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    recipe = ds.produce_recipe([FILE_LIST_PAIRED_1], [FILE_LIST_2_FILES])
+
+@raises(SystemExit)
+def test_dummy_step_produce_recipe_invalid_output_pair():
+    ds = DummyStep([VALID_CONFIG_FILE_1])
+    recipe = ds.produce_recipe([FILE_LIST_PAIRED_1], [FILE_LIST_PAIRED_1])
